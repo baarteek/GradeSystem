6948d2f649fbb8fd26a95b8ab632326fe3820e54
Bartłomiej Krukowski <bartekkrukowski822@gmail.com>
2023-03-23 18:58:55 +0100

    add validator of data entered during registartion

---
+++ src/main/java/com/gradingsystem/utils/Validator.java
@@ -0,0 +1,46 @@
+package com.gradingsystem.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+    public static boolean validateEmail(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean validatePassword(String password) {
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public static boolean validatePESEL(String pesel) {
+        if(pesel.length() != 11) {
+            return false;
+        }
+        for(int i = 0; i < 11; i++) {
+            if (!Character.isDigit(pesel.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean validatePhoneNumber(String phoneNumber) {
+        if (phoneNumber == null || phoneNumber.length() < 9) {
+            return false;
+        }
+        for (int i = 0; i < phoneNumber.length(); i++) {
+            if (!Character.isDigit(phoneNumber.charAt(i)) && phoneNumber.charAt(i) != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
