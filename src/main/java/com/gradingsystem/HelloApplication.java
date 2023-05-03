4c8283132fb841d159b71f10928d2b1a0c029584
p0js <ztraktor2@gmail.com>
2023-05-03 18:31:16 +0200

    Test client and server sockets

--- src/main/java/com/gradingsystem/HelloApplication.java
+++ src/main/java/com/gradingsystem/HelloApplication.java
@@ -7,10 +7,30 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // test connection client-server
+        String host = "localhost";
+        int port = 1000;
+        Socket socket = new Socket(host, port);
+
+        OutputStream outputStream = socket.getOutputStream();
+        String message = "Hello, server!";
+        outputStream.write(message.getBytes());
+
+        InputStream inputStream = socket.getInputStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead = inputStream.read(buffer);
+        String response = new String(buffer, 0, bytesRead);
+        System.out.println("Received response: " + response);
+
+        socket.close();
+
         try {
             Parent root = FXMLLoader.load(getClass().getResource("views/login-view.fxml"));
             Scene loginScene = new Scene(root, 1600, 800);
--- src/main/java/com/gradingsystem/server/Main.java
+++ src/main/java/com/gradingsystem/server/Main.java
@@ -1,9 +1,48 @@
 package com.gradingsystem.server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
         Database.connect();
         Database.create_tables();
-        Database.disconnect();
+
+        try {
+            int port = 1000;
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread thread = new Thread(() -> {
+                    try {
+                        InputStream inputStream = clientSocket.getInputStream();
+                        OutputStream outputStream = clientSocket.getOutputStream();
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = inputStream.read(buffer);
+                        String message = new String(buffer, 0, bytesRead);
+                        System.out.println("Received message: " + message);
+
+                        String response = "Hello client!";
+                        outputStream.write(response.getBytes());
+
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+                thread.start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Database.disconnect();
+        }
     }
 }
+
+
