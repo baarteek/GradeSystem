3fdbd731b9f8205e74d050e46a916910aed85d73
Bartłomiej Krukowski <bartekkrukowski822@gmail.com>
2023-05-17 14:21:14 +0200

    improved readability of the server code

--- src/main/java/com/gradingsystem/server/Main.java
+++ src/main/java/com/gradingsystem/server/Main.java
@@ -33,82 +33,28 @@ public class Main {
                         String[] parts = message.split("\\|");
                         String operationCode = parts[0];
 
-                        if(operationCode.equals("LOGIN")) {
-                            String email = parts[1];
-                            String password = parts[2];
-                            String loginResult = Database.checkCredentials(email, password);
-                            System.out.println(loginResult);
-                            bufferedWriter.write(loginResult);
-                            bufferedWriter.flush();
-                        } else if (operationCode.equals("REGISTER_TEACHER") || operationCode.equals("REGISTER_STUDENT")) {
-                            String name = parts[1];
-                            String surname = parts[2];
-                            String email = parts[3];
-                            String password = parts[4];
-                            String pesel = parts[5];
-                            String phoneNumber = parts[6];
-                            String[] columnNames = {"pesel", "email", "telefon"};
-                            String[] values = {pesel, email, phoneNumber};
-                            String registerResult = new String();
-                            if(operationCode.equals("REGISTER_TEACHER")) {
-                                String tableName = "nauczyciel";
-                                registerResult = Database.checkIfDataExists(tableName, columnNames, values);
-                                if(registerResult.equals("SUCCESS")) {
-                                    Database.add_nauczyciel(name, surname, pesel, email, phoneNumber, password);
-                                }
-                            }
-                            if(operationCode.equals("REGISTER_STUDENT")) {
-                                String tableName = "uczen";
-                                registerResult = Database.checkIfDataExists(tableName, columnNames, values);
-                                if(registerResult.equals("SUCCESS")) {
-                                    Database.add_uczen(1, name, surname, pesel, email, phoneNumber, password);
-                                }
-                            }
-                            String mess = "REGISTER_" + registerResult;
-                            bufferedWriter.write(mess);
-                            bufferedWriter.flush();
-                        } else if(operationCode.equals("GET_USER_DATA")) {
-                            int userID = Integer.parseInt(parts[2]);
-                            String findUserResult = new String();
-                            if(parts[1].equals("TEACHER")) {
-                                findUserResult = Database.getUserDataById("nauczyciel", userID);
-                            } else if (parts[1].equals("STUDENT")) {
-                                findUserResult = Database.getUserDataById("uczen", userID);
-                            }
-                            String mess = new String();
-                            if(findUserResult == null) {
-                                mess = "GET_USER_DATA_FAILURE";
-                            } else {
-                                mess = "GET_USER_DATA_SUCCESS|" + findUserResult;
-                            }
-                            System.out.println(mess);
-                            bufferedWriter.write(mess);
-                            bufferedWriter.flush();
-                        } else if (operationCode.equals("CHANGE_USER_DATA")) {
-                            int userID = Integer.parseInt(parts[3]);
-                            String column = parts[2];
-                            String value = parts[4];
-                            String changeUserDataResult = new String();
-                            if(parts[1].equals("TEACHER")) {
-                                changeUserDataResult = Database.changeUserData("nauczyciel", userID, column, value);
-                            } else if (parts[1].equals("STUDENT")) {
-                                changeUserDataResult = Database.changeUserData("uczen", userID, column, value);
-                            }
-                            bufferedWriter.write(changeUserDataResult);
-                            bufferedWriter.flush();
-                        } else if(operationCode.equals("CHECK_DATA_EXISTT")){
-                            String userDataResult = new String();
-                            String columnNames = parts[2];
-                            String values = parts[3];
-                            if(parts[1].equals("TEACHER")) {
-                                userDataResult = Database.checkIfDataExists("nauczyciel", new String[]{columnNames}, new String[]{values});
-                            } else if (parts[1].equals("STUDENT")) {
-                                userDataResult = Database.checkIfDataExists("uczen", new String[]{columnNames}, new String[]{values});
-                            }
-                            bufferedWriter.write(userDataResult);
-                            bufferedWriter.flush();
+                        String response = "";
+                        switch (operationCode) {
+                            case "LOGIN":
+                                response = handleLogin(parts);
+                                break;
+                            case "REGISTER_TEACHER":
+                            case "REGISTER_STUDENT":
+                                response = handleRegister(parts, operationCode);
+                                break;
+                            case "GET_USER_DATA":
+                                response = handleGetUserData(parts);
+                                break;
+                            case "CHANGE_USER_DATA":
+                                response = handleChangeUserData(parts);
+                                break;
+                            case "CHECK_DATA_EXISTT":
+                                response = handleCheckDataExist(parts);
+                                break;
                         }
 
+                        bufferedWriter.write(response);
+                        bufferedWriter.flush();
                         clientSocket.close();
                     } catch (IOException e) {
                         throw new RuntimeException(e);
@@ -123,6 +69,77 @@ public class Main {
             Database.disconnect();
         }
     }
+
+    private static String handleLogin(String[] parts) {
+        String email = parts[1];
+        String password = parts[2];
+        return Database.checkCredentials(email, password);
+    }
+
+    private static String handleRegister(String[] parts, String operationCode) {
+        String name = parts[1];
+        String surname = parts[2];
+        String email = parts[3];
+        String password = parts[4];
+        String pesel = parts[5];
+        String phoneNumber = parts[6];
+        String[] columnNames = {"pesel", "email", "telefon"};
+        String[] values = {pesel, email, phoneNumber};
+        String registerResult = "";
+        if (operationCode.equals("REGISTER_TEACHER")) {
+            String tableName = "nauczyciel";
+            registerResult = Database.checkIfDataExists(tableName, columnNames, values);
+            if (registerResult.equals("SUCCESS")) {
+                Database.add_nauczyciel(name, surname, pesel, email, phoneNumber, password);
+            }
+        }
+        if (operationCode.equals("REGISTER_STUDENT")) {
+            String tableName = "uczen";
+            registerResult = Database.checkIfDataExists(tableName, columnNames, values);
+            if (registerResult.equals("SUCCESS")) {
+                Database.add_uczen(1, name, surname, pesel, email, phoneNumber, password);
+            }
+        }
+        return "REGISTER_" + registerResult;
+    }
+
+    private static String handleGetUserData(String[] parts) {
+        int userID = Integer.parseInt(parts[2]);
+        String findUserResult = "";
+        if (parts[1].equals("TEACHER")) {
+            findUserResult = Database.getUserDataById("nauczyciel", userID);
+        } else if (parts[1].equals("STUDENT")) {
+            findUserResult = Database.getUserDataById("uczen", userID);
+        }
+        if (findUserResult == null) {
+            return "GET_USER_DATA_FAILURE";
+        } else {
+            return "GET_USER_DATA_SUCCESS|" + findUserResult;
+        }
+    }
+
+    private static String handleChangeUserData(String[] parts) {
+        int userID = Integer.parseInt(parts[3]);
+        String column = parts[2];
+        String value = parts[4];
+        String changeUserDataResult = "";
+        if (parts[1].equals("TEACHER")) {
+            changeUserDataResult = Database.changeUserData("nauczyciel", userID, column, value);
+        } else if (parts[1].equals("STUDENT")) {
+            changeUserDataResult = Database.changeUserData("uczen", userID, column, value);
+        }
+        return changeUserDataResult;
+    }
+
+    private static String handleCheckDataExist(String[] parts) {
+        String columnNames = parts[2];
+        String values = parts[3];
+        String userDataResult = "";
+        if (parts[1].equals("TEACHER")) {
+            userDataResult = Database.checkIfDataExists("nauczyciel", new String[]{columnNames}, new String[]{values});
+        } else if (parts[1].equals("STUDENT")) {
+            userDataResult = Database.checkIfDataExists("uczen", new String[]{columnNames}, new String[]{values});
+        }
+        return userDataResult;
+    }
 }
-
-
